e13207ba082aa916090380c53da6513c2b828087
broma186 <mattbr@hotmail.co.nz>
2015-07-28 15:28:32 +1200

    Initial Commit

---
+++ Asteroid Quest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Asteroid Quest
+//
+//  Created by Matt on 28/07/15.
+//  Copyright (c) 2015 Matthew Brooker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Asteroid Quest/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Asteroid Quest
+//
+
+//  Copyright (c) 2015 Matthew Brooker. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
